f65fa420776f292d49c3c291c726affdab1680e5
zacw-243L <qwerty.amousy123442x@gmail.com>
2025-01-14 10:37:48 +0800

    Update git_commit_inflator.bat

--- git_commit_inflator.bat
+++ git_commit_inflator.bat
@@ -2,8 +2,14 @@
 :: Change directory to the specified Git repository
 cd "C:\Github staging area\git comit inflator"
 
+:: Ensure the branch name is correct (replace master with your branch if needed)
+SET BRANCH=master
+
 :: Loop to continuously update the file, commit, and push
 :loop
+    :: Suppress LF to CRLF warnings for this repository
+    git config core.autocrlf false
+
     :: Generate a random line of text and append it to the file
     echo %RANDOM% >> file.txt
 
@@ -13,8 +19,8 @@ cd "C:\Github staging area\git comit inflator"
     :: Commit the changes with a message including the current timestamp
     git commit -m "Update made at %date% %time%"
 
-    :: Push the commit to the remote repository
-    git push origin main
+    :: Push the commit to the correct branch
+    git push origin %BRANCH%
 
     :: Wait for 1 second before the next iteration
     timeout /t 1 >nul
