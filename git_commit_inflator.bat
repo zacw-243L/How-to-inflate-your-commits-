5b9da2e1231246f28547f393b1eb829b58d51773
zacw-243L <qwerty.amousy123442x@gmail.com>
2025-05-01 11:41:00 +0800

    changed something again

--- git_commit_inflator.bat
+++ git_commit_inflator.bat
@@ -5,19 +5,30 @@ cd "C:\Github staging area\How-to-inflate-your-commits-"
 :: Ensure the branch name is correct (replace master with your branch if needed)
 SET BRANCH=master
 
-:: Loop to continuously update the file, commit, and push
+:: Loop to continuously update the files, commit, and push
 :loop
     :: Suppress LF to CRLF warnings for this repository
     git config core.autocrlf false
 
-    :: Generate a random line of text and append it to the file
+    :: Generate a random line of text and append it to the text file
     echo %RANDOM% >> file.txt
 
-    :: Stage the file
+    :: Generate a random Python comment and append it to the Python file
+    SET /A rand=%RANDOM% %% 5
+    IF %rand%==0 SET COMMENT=# TODO: add error handling
+    IF %rand%==1 SET COMMENT=# FIXME: improve performance
+    IF %rand%==2 SET COMMENT=# HACK: temporary patch
+    IF %rand%==3 SET COMMENT=# REVIEW: check logic flow
+    IF %rand%==4 SET COMMENT=# NOTE: optimize later
+
+    echo %COMMENT% >> "file.py"
+
+    :: Stage both files
     git add file.txt
+    git add file.py
 
     :: Commit the changes with a message including the current timestamp
-    git commit -m "Update made at %date% %time%"
+    git commit -m "Inflated commit at %date% %time%"
 
     :: Push the commit to the correct branch
     git push origin %BRANCH%
